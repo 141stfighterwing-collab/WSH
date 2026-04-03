7d19d2d823205ec4d458f93e39e2a2b13c12dc3a
Z User <z@container>
2026-04-03 22:50:59 +0000

    4aa015b3-402a-4914-b9b8-0cb89c5f130b

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
 
         {/* Main Content */}
         <main className="flex-1 overflow-y-auto min-w-0">
-          <div className="max-w-4xl mx-auto px-2 py-2 md:px-4 md:py-3">
+          <div className="px-2 py-2 md:px-4 md:py-3">
             {/* Editor */}
             <NoteEditor />
 
